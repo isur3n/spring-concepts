75ab429ee75cab6476d066c4929135aa16c7fefd
Surendra Raut <surendra.raut@gmail.com>
2024-03-09 16:04:10 -0500

    Annotated methods scanning added.

--- src/main/java/me/suren/springconcepts/app/aspect/TimeItInterceptor.java
+++ src/main/java/me/suren/springconcepts/app/aspect/TimeItInterceptor.java
@@ -1,22 +1,38 @@
 package me.suren.springconcepts.app.aspect;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import me.suren.springconcepts.app.annotation.TimeIt;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
 
 @Slf4j
 @Aspect
 @Component
 public class TimeItInterceptor {
 
-    @Around("@annotation(TimeIt)")
+    private Set<Method> methods = new HashSet<>();
+
+    @PostConstruct
+    public void init() {
+        Reflections reflections = new Reflections("me.suren", new MethodAnnotationsScanner());
+        methods.addAll(reflections.getMethodsAnnotatedWith(TimeIt.class));
+    }
+
+    @Around("@annotation(me.suren.springconcepts.app.annotation.TimeIt)")
     public Object executeAround(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         Object response = joinPoint.proceed();
         long executionTime = System.currentTimeMillis() - start;
-        log.info(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        log.info(joinPoint.getSignature().toLongString() + " executed in " + executionTime + "ms");
 
         return response;
     }
