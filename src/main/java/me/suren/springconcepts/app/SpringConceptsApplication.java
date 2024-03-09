a3763bf6b014efd0f97edfe944d8af821e15837a
Surendra Raut <surendra.raut@gmail.com>
2024-03-08 20:23:32 -0500

    Moved main class in new package.

--- src/main/java/me/suren/springconcepts/app/SpringConceptsApplication.java
+++ src/main/java/me/suren/springconcepts/app/SpringConceptsApplication.java
@@ -1,4 +1,4 @@
-package me.suren.springconcepts;
+package me.suren.springconcepts.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
